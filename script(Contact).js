a161887b110c43f108b50468d65c836e051ebecd
Lewisg987 <146995487+Lewisg987@users.noreply.github.com>
2023-10-06 01:29:39 +0100

    Add files via upload

---
+++ script(Contact).js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var navMenu = document.getElementById("navMenu");
+    navMenu.classList.toggle("active");
+    const hamburger = document.querySelector('.hamburger');
+    hamburger.classList.toggle('active');
+}
---
+++ script(Cricket).js
@@ -0,0 +1,19 @@
+function toggleMenu() {
+    var navMenu = document.getElementById("navMenu");
+    navMenu.classList.toggle("active");
+    const hamburger = document.querySelector('.hamburger');
+    hamburger.classList.toggle('active');
+}
+
+window.addEventListener('scroll', function() {
+    // Change text after scrolling 500 pixels
+    var scrollThreshold = 1000; // Adjust this value to change when the text changes
+    var changingText = document.getElementById('text');
+  
+    if (window.pageYOffset >= scrollThreshold) {
+      changingText.innerText = " \n Other info"; // Change to the desired text
+    } else {
+      changingText.innerText = " \n Cricket"; // Change to the original text
+    }
+  });
+  
